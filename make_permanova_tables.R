3fecab1760c4d99f8f5911d29a0c6d1da6cbebc0
johnchaston <john_chaston@byu.edu>
2025-06-30 12:38:13 -0600

    Update make_permanova_tables.R
    updates to adonis2 2015

--- make_permanova_tables.R
+++ make_permanova_tables.R
@@ -8,7 +8,7 @@ i="unweighted_unifrac"
   assign(x = paste0("flies_",i,"_dm"), value = as.dist(get(paste0("flies_",i))[,2:dim(get(paste0("flies_",i)))[2]]))
   flies_unifrac_table <- get(paste0("flies_",i)) %>% dplyr::select(X) %>% left_join(read.table(mapper_file,comment.char = "", header=T, fill=T, sep="\t"), by=c("X"="X.SampleID")) %>% droplevels()
   set.seed(seed_to_set)
-  assign(x = paste0("f_",i,"_permanova"), adonis2(as.formula(paste0("flies_",i,"_dm ",pform)), flies_unifrac_table, permutations=1000))
+  assign(x = paste0("f_",i,"_permanova"), adonis2(as.formula(paste0("flies_",i,"_dm ",pform)), flies_unifrac_table, permutations=1000, by = "terms"))
   assign(x = paste0("fig_",i), value = tableGrob(data.frame(Df = round(get(paste0("f_",i,"_permanova"))$Df, 2), SS = round(get(paste0("f_",i,"_permanova"))$SumOfSqs, 2), R2 = round(get(paste0("f_",i,"_permanova"))$R2, 2), Fval = round(get(paste0("f_",i,"_permanova"))$`F`, 2), p = round(get(paste0("f_",i,"_permanova"))$`Pr(>F)`, 2)),theme=ttheme_minimal()))
   plot(get(paste0("fig_",i)))
   write.table(i, file = paste0('core-metrics-results-',file_path,"/",file_path,"_permanova_table.txt"), append = T, quote = F, sep = "\t", row.names = F, col.names = F)
